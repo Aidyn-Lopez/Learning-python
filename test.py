7c2b050da7b85ab7ffc485a140781033783b91e1
Aidyn Lopez <aidyn.dru.lopez03@gmail.com>
2021-11-26 22:38:57 -0500

    project 1

---
+++ test.py
@@ -0,0 +1,27 @@
+class Test:
+    """This is a Test."""
+
+    def __init__(self,
+                 test_number,
+                 test_name):
+        self.test_number = test_number
+        self.test_name = test_name
+
+    def write_test_info(self):
+        info = f"The name of the test is {self.test_name}: {self.test_number}"
+        print(info)
+        return self.test_number + 1
+
+
+test1 = Test(1, "Firstly,")
+answer1 = test1.write_test_info()
+print(f"the next test is: {answer1}")
+test2 = Test(2, "Secondly,")
+answer2 = test2.write_test_info()
+print(f"the next test is: {answer2}")
+test3 = Test(3, "Thirdly,")
+answer3 = test3.write_test_info()
+print(f"the next test is: {answer3}")
+test4 = Test(4, "Fourthly")
+answer4 = test4.write_test_info()
+print(f"the next test is: {answer4}")
