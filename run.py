0041c757d5f863cb74087ab832038cb836ff1b17
Aidyn Lopez <aidyn.dru.lopez03@gmail.com>
2021-11-26 23:03:45 -0500

    made package for class Test and put module within package.

---
+++ run.py
@@ -0,0 +1,14 @@
+from package_test.test import Test
+
+test1 = Test(1, "Firstly,")
+answer1 = test1.write_test_info()
+print(f"the next test is: {answer1}")
+test2 = Test(2, "Secondly,")
+answer2 = test2.write_test_info()
+print(f"the next test is: {answer2}")
+test3 = Test(3, "Thirdly,")
+answer3 = test3.write_test_info()
+print(f"the next test is: {answer3}")
+test4 = Test(4, "Fourthly")
+answer4 = test4.write_test_info()
+print(f"the next test is: {answer4}")
